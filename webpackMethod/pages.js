545befd464dc522bb91e9c62ab9a08a677681178
QJ张金龙 <wo_99936@qq.com>
2020-12-24 11:37:02 +0800

    更改多页面配置

--- webpackMethod/pages.js
+++ webpackMethod/pages.js
@@ -1,22 +1,26 @@
 const glob = require('glob');
+const fs = require("fs");
 
 let titles = {
-    
+
 };
 
-function getEntry(globPath) {
+const getEntry = (globPath) => {
     let entries = {}, tmp, htmls = {};
-    glob.sync(globPath+'html').forEach(function(entry) {
+    glob.sync(globPath+'html').forEach((entry) => {
         tmp = entry.split('/').splice(-3);
         htmls[tmp[1]] = entry
     })
-    glob.sync(globPath+'ts').forEach(function(entry) {
+    glob.sync(`${globPath}ts`).forEach((entry) => {
         tmp = entry.split('/').splice(-3);
+        let templatePath = entry.replace("main.ts","index.html");
+        const isTemplate = fs.existsSync(templatePath);
+        let template = isTemplate?templatePath:'index.html';
         entries[tmp[1]] = {
             entry,
-            template: 'index.html', 
-            filename:tmp[1] + '.html',
-            title:titles[tmp[1]] || ""
+            template, 
+            filename:`${tmp[1]}.html`,
+            title:titles[tmp[1]] || "未命名模块"
         };
     });
     return entries;
